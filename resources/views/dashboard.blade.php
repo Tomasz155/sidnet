e3f8977ecb01ee1618b09e8505150db09234f248
Tomasz155 <it@mdtrading.pl>
2021-03-11 15:40:43 +0000

    2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,23 +4,24 @@
 
     <div>
         <div class="container d-flex h-flex align-items-center" >
-            <div class="mx-auto text-center">
-                <table class="table table-bordered">
-                    <thead>
-                      <tr>
-                        <th scope="col">Books</th>
-                        <th scope="col">Authors</th>
-                        <th scope="col">Publishing houses</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td><a href="/books" class="href">{{ $stats['countBooks'] }}</a></td>
-                        <td><a href="/authors" class="href">{{ $stats['countAuthors'] }}</a></td>
-                        <td><a href="/publishers" class="href">{{ $stats['countPublishers'] }}</a></td>
-                      </tr>
-                    </tbody>
-                  </table>
+            <div class="mx-auto text-center">   
+              <h1 class="mt-4">Dashboard</h1>            
+              <table class="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th scope="col">Books</th>
+                      <th scope="col">Authors</th>
+                      <th scope="col">Publishing houses</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td><a href="/books" class="href">{{ $stats['countBooks'] }}</a></td>
+                      <td><a href="/authors" class="href">{{ $stats['countAuthors'] }}</a></td>
+                      <td><a href="/publishers" class="href">{{ $stats['countPublishers'] }}</a></td>
+                    </tr>
+                  </tbody>
+                </table>
             </div>
         </div>
     </div>
